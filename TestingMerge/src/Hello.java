5656c3567c321072207230dbdd1eaef4e9d966be
Adil <jaberadil.rahman@Audatex.co.uk>
2017-11-30 14:51:28 +0000

    Modfied project to be merged with the sub branch "MergeIntoMaster local Repo"

--- TestingMerge/src/Hello.java
+++ TestingMerge/src/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, I am testing git merge!");
-		System.out.println("I like cats");
+		System.out.println("I like dogs");
+		System.out.println("There should be conflict above");
+		
 
 	}
 
